26ada2674d208dbfe9b073a8002859522a04dbb0
Janya ward <janyaward33@gmail.com>
2025-09-30 14:21:31 -0500

    Get index for age, bmi, children, and charges columns

--- GetInfo.java
+++ GetInfo.java
@@ -12,7 +12,20 @@ public class GetInfo {
                 System.err.println("File is empty: " + filename);
                 return;
             }
-            System.out.println("Header: " + headerLine);
+
+            String[] headers = headerLine.split(",");
+            int ageIndex = -1, bmiIndex = -1, childrenIndex = -1, chargesIndex = -1;
+
+            for (int i = 0; i < headers.length; i++) {
+                String col = headers[i].trim().toLowerCase();
+                if (col.equals("age")) ageIndex = i;
+                else if (col.equals("bmi")) bmiIndex = i;
+                else if (col.equals("children")) childrenIndex = i;
+                else if (col.equals("charges")) chargesIndex = i;
+            }
+
+            System.out.printf("Indices -> age: %d, bmi: %d, children: %d, charges: %d%n",
+                              ageIndex, bmiIndex, childrenIndex, chargesIndex);
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
             e.printStackTrace();
